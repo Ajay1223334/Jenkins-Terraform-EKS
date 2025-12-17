85bec73c022f03d8b5822da2e89362def137238f
Ajay1223334 <apigeedu5@gmail.com>
2025-12-17 22:05:51 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -35,8 +35,8 @@ module "eks" {
       max_size     = 2
       desired_size = 1
 
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
+      instance_types = ["t3.micro"]
+capacity_type  = "ON_DEMAND"
 
       tags = {
         ExtraTag = "helloworld"
